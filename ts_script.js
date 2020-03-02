3252ae94738afd0cfda20ae49db8dee70e69b4f6
ktpi2000 <katapi2000@gmail.com>
2020-03-02 16:00:27 +0900

    テンプレートリテラル

--- ts_script.js
+++ ts_script.js
@@ -1,4 +1,8 @@
-var msg = 'Hello! This is Typescript!';
+var lang = 'Javascript';
+var msg1 = "Hello! This is " + lang + "!";
+lang = 'Typescript';
+var msg2 = "Hello! This is " + lang + "!";
+var msg = "\u300C" + msg1 + "\u300D<br>\u300C" + msg2 + "\u300D";
 function load() {
-    document.querySelector('#msg').textContent = msg;
+    document.querySelector('#msg').innerHTML = msg;
 }
--- ts_script.ts
+++ ts_script.ts
@@ -1,5 +1,10 @@
-let msg = 'Hello! This is Typescript!';
+let lang = 'Javascript';
+let msg1 = `Hello! This is ${lang}!`;
+lang = 'Typescript';
+let msg2 = `Hello! This is ${lang}!`;
+
+let msg: string = `「${msg1}」<br>「${msg2}」`;
 
 function load() {
-    document.querySelector('#msg').textContent = msg;
+    document.querySelector('#msg').innerHTML = msg;
 }
