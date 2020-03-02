63d9aaa936cc1d79844e5dd7d0d82e3ddcf7eb32
ktpi2000 <katapi2000@gmail.com>
2020-03-02 16:36:16 +0900

    コンストラクタ関数

--- ts_script.js
+++ ts_script.js
@@ -1,11 +1,14 @@
-var ob = {
-    name: 'Taro',
-    mail: 'taro@yamada',
-    age: 35,
-    toString: function () {
+function Person(nm, ml, ag) {
+    this.name = nm;
+    this.mail = ml;
+    this.age = ag;
+    this.toString = function () {
         return this.name + ',' + this.mail + ',' + this.age;
-    }
-};
+    };
+}
+var ob1 = new Person('taro', 'taro@yamada', 40);
+var ob2 = new Person('suzuki', 'suzuki@honda', 30);
+var msg = ob1 + " <br> " + ob2;
 function load() {
-    document.querySelector('#msg').innerHTML = ob.toString();
+    document.querySelector('#msg').innerHTML = msg;
 }
