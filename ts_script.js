d310e0cd1777237d2e70e4887ea24a463a109efc
ktpi2000 <katapi2000@gmail.com>
2020-03-04 08:26:49 +0900

    継承

--- ts_script.js
+++ ts_script.js
@@ -14,7 +14,27 @@ class Person {
         return this.name + ',' + this.mail + ',' + this.age;
     }
 }
-var ob = new Person('taro', 'taro@gmail.com', 80);
+class Teen extends Person {
+    constructor(nm, ml, ag, sc) {
+        super(nm, ml, ag); //Personクラスのコンストラクタを呼び出し
+        this.school = sc;
+    }
+    get age() {
+        return this.__age;
+    }
+    set age(n) {
+        this.__age = n < 0 ? 0 : n >= 20 ? 19 : n;
+    }
+    toString() {
+        return this.name + ',' + this.mail + ',' + this.age + ' (' + this.school + ')';
+    }
+}
+let taro = new Person('taro', 'taro@gmail.com', 80);
+let jiro = new Teen('jiro', 'jiro@gmail.com', 17, 'oyama kosen');
 function load() {
-    document.querySelector('#msg').innerHTML = ob.toString();
+    let msg = `<ul>
+    <li>${taro.toString()}</li>
+    <li>${jiro.toString()}</li>
+    </ul>`;
+    document.querySelector('#msg').innerHTML = msg;
 }
